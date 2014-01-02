1ee92cb830907b70750799d1c770617d7a9648b2
magicsuny <thirteenmagic@gmail.com>
2014-01-02 20:28:11 +0800

    update

--- Server/router/validate/index.js
+++ Server/router/validate/index.js
@@ -1,16 +1,23 @@
 var crypto=require("crypto"),
-    https = require('https');
+    https = require('https'),
+    xml=require("../../node_modules/node-xml/lib/node-xml.js");
 var APP_ID='wxbf10215040495193',
     APP_SECRET='a251ff67a3e923214086549cd3e6e532',
     TOKEN='magic';
 exports.index = function(req,res){
     var query = req.getData;
+
     if(query){
         var echostr = checkSignature(query);
         if(echostr){
             console.log(req.post);
             requestAccessToken();
-            res.end(echostr);
+            if(req.method=='GET'){
+                res.end(echostr);
+            }else{
+                processMessage(req.post,res);
+            }
+
         }else{
             res.end("Bad Token!");
         }
@@ -123,4 +130,88 @@ function createMenu(accessToken){
     req.on('error', function(e) {
         console.error(e);
     });
+}
+
+function processMessage(data,response){
+    var ToUserName="";
+    var FromUserName="";
+    var CreateTime="";
+    var MsgType="";
+    var Content="";
+    var Location_X="";
+    var Location_Y="";
+    var Scale=1;
+    var Label="";
+    var PicUrl="";
+    var FuncFlag="";
+
+    var tempName="";
+    var parse=new xml.SaxParser(function(cb){
+        cb.onStartElementNS(function(elem,attra,prefix,uri,namespaces){
+            tempName=elem;
+        });
+
+        cb.onCharacters(function(chars){
+            chars=chars.replace(/(^\s*)|(\s*$)/g, "");
+            if(tempName=="CreateTime"){
+                CreateTime=chars;
+            }else if(tempName=="Location_X"){
+                Location_X=cdata;
+            }else if(tempName=="Location_Y"){
+                Location_Y=cdata;
+            }else if(tempName=="Scale"){
+                Scale=cdata;
+            }
+
+
+        });
+
+        cb.onCdata(function(cdata){
+
+            if(tempName=="ToUserName"){
+                ToUserName=cdata;
+            }else if(tempName=="FromUserName"){
+                FromUserName=cdata;
+            }else if(tempName=="MsgType"){
+                MsgType=cdata;
+            }else if(tempName=="Content"){
+                Content=cdata;
+            }else if(tempName=="PicUrl"){
+                PicUrl=cdata;
+            }else if(tempName=="Label"){
+                Label=cdata;
+            }
+            console.log("cdata:"+cdata);
+        });
+
+        cb.onEndElementNS(function(elem,prefix,uri){
+            tempName="";
+        });
+
+        cb.onEndDocument(function(){
+            console.log("onEndDocument");
+            tempName="";
+            var date=new Date();
+            var yy=date.getYear();
+            var MM=date.getMonth() + 1;
+            var dd=date.getDay();
+            var hh=date.getHours();
+            var mm=date.getMinutes();
+            var ss=date.getSeconds();
+            var sss=date.getMilliseconds();
+            var result=Date.UTC(yy,MM,dd,hh,mm,ss,sss);
+            var msg="";
+            if(MsgType=="text"){
+                msg="谢谢关注,你说的是："+Content;
+            }else if (MsgType="location"){
+                msg="你所在的位置: 经度："+Location_X+"纬度："+Location_Y;
+            }else if (MsgType="image"){
+                msg="你发的图片是："+PicUrl;
+            }
+            messageSender.sendTextMessage(FromUserName,ToUserName,CreateTime,msg,FuncFlag,response);
+            res.end('<xml><ToUserName><![CDATA['+FromUserName+']]></ToUserName><FromUserName><![CDATA['+ToUserName+']]></FromUserName><CreateTime>'+CreateTime+'</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[你好]]></Content></xml>');
+
+        });
+    });
+    parse.parseString(data);
 }
