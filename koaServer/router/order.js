91490070e591d698c02fe3cd05df1aa654e18cb3
magicsuny <thirteenmagic@gmail.com>
2014-08-06 14:39:25 +0800

    update

--- koaServer/router/order.js
+++ koaServer/router/order.js
@@ -2,13 +2,15 @@
  * Created by sunharuka on 14-7-25.
  */
 var parse = require('co-body');
+var url = require('url');
 var render = require('../libs/render');
 var CarInfoDAO = require('../dao/carInfoDAO');
 function OrderHandler (db) {
   var carInfoDAO = new CarInfoDAO(db);
 
   this.newOrder = function *(){
-    this.session.test = 1;
+    var query = url.parse(this.req.url,true).query;
+    console.log(query);
     var carBrands = yield carInfoDAO.getCarBrand();
     this.body = yield render('/order/neworder',{carBrands:carBrands});
   }
