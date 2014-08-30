ab341abe62fffbc497fafa31a136735b3c161898
magicsuny <thirteenmagic@gmail.com>
2014-08-30 18:29:29 +0800

    config

--- koaServer/config.js
+++ koaServer/config.js
@@ -7,7 +7,7 @@ module.exports = {
     db:'car',
     host:'127.0.0.1'
   },
-  port:8080,
+  port:80,
   weixin:{
     APP_ID:'wxf72e9f4c5958f96d',
     APP_SECRET:'bcdac03ae24bd1f04a462b2baf20cc04',
